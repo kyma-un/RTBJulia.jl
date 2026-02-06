02bf66cb2265fa29d85aaabb60ac74bae98c9505
JSDaleman <jdaleman@unal.edu.co>
2026-02-06 15:17:55 -0500

    test: Prueba de cración de test

--- src/base/se2.jl
+++ src/base/se2.jl
@@ -2,13 +2,13 @@ module SE2
 
 using StaticArrays
 
-export se2
+export Se2
 
 # ============================================================
 # SE(2) homogeneous transformation
 # ============================================================
 
-function se2(x::T, y::T, θ::T; deg::Bool=false) where T
+function Se2(x::T, y::T, θ::T; deg::Bool=false) where T
     θ = deg ? θ * (pi / one(T) / 180) : θ
 
     c = cos(θ)
@@ -22,15 +22,15 @@ function se2(x::T, y::T, θ::T; deg::Bool=false) where T
 end
 
 # se2([x, y])
-se2(xy::SVector{2,T}; deg::Bool=false) where T =
-    se2(xy[1], xy[2], zero(T); deg=deg)
+Se2(xy::SVector{2,T}; deg::Bool=false) where T =
+    Se2(xy[1], xy[2], zero(T); deg=deg)
 
 # se2([x, y], θ)
-se2(xy::SVector{2,T}, θ::T; deg::Bool=false) where T =
-    se2(xy[1], xy[2], θ; deg=deg)
+Se2(xy::SVector{2,T}, θ::T; deg::Bool=false) where T =
+    Se2(xy[1], xy[2], θ; deg=deg)
 
 # se2([x, y, θ])
-se2(xyt::SVector{3,T}; deg::Bool=false) where T =
-    se2(xyt[1], xyt[2], xyt[3]; deg=deg)
+Se2(xyt::SVector{3,T}; deg::Bool=false) where T =
+    Se2(xyt[1], xyt[2], xyt[3]; deg=deg)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "RTBJulia.jl" begin
     # Write your tests here.
+    include("test_se2.jl")
 end
